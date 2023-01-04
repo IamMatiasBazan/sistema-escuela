7b5f04901858efb7c8c3788f853431b0f7b73d3a
[DMatias <[Dbazanmatias2004@gmail.com>
2023-01-04 18:18:07 -0300

    trayendo tablas en una variable

--- database/abrir_conexion.php
+++ database/abrir_conexion.php
@@ -1,11 +1,22 @@
 <?php
-$host="127.0.0.1";
-$port=3306;
-$socket="";
-$user="root";
-$password="";
-$dbname="escuela";
+    $host="127.0.0.1";
+    $port=3306;
+    $socket="";
+    $user="root";
+    $password="";
+    $dbname="escuela";
 
-$conexion = new mysqli($host, $user, $password, $dbname, $port, $socket)
+    $conexion = new mysqli($host, $user, $password, $dbname, $port, $socket);
+
+    $tabla1 = "clases";
+    $tabla2 = "materias";
+    $tabla3 = "roles";
+    $tabla4 = "usuarios";
+
+    if ($conexion -> connect_errno) {
+        echo "Nuestro sitio esta experimentando fallos";
+        exit();
+    }
+  
 
 ?>
