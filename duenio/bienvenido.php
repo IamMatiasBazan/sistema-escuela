81a8adcb0e35605a215ed3f9f1e2517a4bdce158
[DMatias <[Dbazanmatias2004@gmail.com>
2023-01-05 19:19:47 -0300

    Primera version

--- duenio/bienvenido.php
+++ duenio/bienvenido.php
@@ -8,6 +8,9 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <!-- CERRANDO BOOTSTRAP -->
+    <!--ABRIENDO GOOGLE FONT-->
+        <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Rubik+Bubbles&display=swap" rel="stylesheet">
+    <!--CERRANDO GOOGLE FONT-->
     <title>Document</title>
     <link rel="stylesheet" href="../css/rellenar.css">
 </head>
@@ -22,7 +25,7 @@
 
         if($nombre == "" || $password == "" ) {
             echo '<div class="alert alert-danger" id="rellenar" role="alert">
-            <center><p id="rellenar_campos">Rellene los campos<a href="../index.php" class="alert-link"> INICIAR SECION.</a></p></center>
+            <center><p id="rellenar_campos">Rellene los campos<a href="../index.php" class="alert-link"> INICIAR SESION.</a></p></center>
           </div>';
         } else {
             $q = "SELECT COUNT(*) AS contar FROM $tabla4 WHERE nombre = '$nombre' AND contrasenia = '$password'";
@@ -30,10 +33,19 @@
             $array = mysqli_fetch_array($consulta);
             
             if($array['contar'] > 0) {
-                echo "<h1>Bienvenido $nombre";
+                echo "<center><h1>Bienvenido $nombre</h1></center>";
+                echo "<div class='menu'>
+                    <div class='parrafos'> 
+                        <p>Crear y asignarles el perfil</p>
+                        <p>Cargar y consulta de las materias</p>
+                        <p>Consulta el monto a pagar a los alumno</p>
+                        <p>Consulta el monto a pagar a los profesores</p>
+                        <p>Consultar clases de profesores y alumno<p>
+                    </div>
+                </div>";
             } else {
             echo '<div class="alert alert-danger" id="completar" role="alert">
-                    <center><p id="completar_campos">Datos incorrectos<a href="../index.php" class="alert-link"> INICIAR SECION.</a></p></center>
+                    <center><p id="completar_campos">Datos incorrectos<a href="../index.php" class="alert-link"> INICIAR SESION.</a></p></center>
                 </div>';
             }
         }
